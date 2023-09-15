00a8cff5932dbb1388d8037b69daab496b2c2668
SARAH PRADO MEDEIROS <SARAH0253@DIGITALCOLLEGE.LOCAL>
2023-09-14 21:55:52 -0300

    tentando fazer um get

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 //criou um servidor
 
-const http = require('http')
+// const http = require('http')
+
+import {get, createServer} from 'http'
 
 const HOST = 'localhost'
 const PORT = 3333
 
-const server = http.createServer((req, res)=> {
+const server = createServer((req, res)=> {
  res.end('oi! estou funcionando yurru')
 })
 
 
 server.listen(PORT, HOST, () => {
     console.log(`Servidor Rodando: http://${HOST}: ${PORT}/`);
+})
+
+get('/', ()=>{
+    return 'Test'
 })
