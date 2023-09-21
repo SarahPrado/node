109d7cf15e7bf46d8241baca740d89ca4df3612f
SARAH PRADO MEDEIROS <SARAH0253@DIGITALCOLLEGE.LOCAL>
2023-09-21 18:48:15 -0300

    http modulo

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ import {get, createServer} from 'http'
 const HOST = 'localhost'
 const PORT = 3333
 
-const server = createServer((req, res)=> {
- res.end('oi! estou funcionando yurru')
+const server = createServer((req, res)=> { 
+ console.log(req.method);
+
+ if(req.method === 'GET'){
+    if(req.url === '/usuarios'){
+        res.writeHead(200, {'Content-type':'text/plain'})
+        res.end('Entrou no /usuarios')
+    }else{
+        res.end('oi! estou funcionando yurru')
+    }
+ }
 })
 
 
@@ -16,6 +25,6 @@ server.listen(PORT, HOST, () => {
     console.log(`Servidor Rodando: http://${HOST}: ${PORT}/`);
 })
 
-get('/', ()=>{
-    return 'Test'
-})
+// get('/', ()=>{
+//     return 'Test'
+// })
